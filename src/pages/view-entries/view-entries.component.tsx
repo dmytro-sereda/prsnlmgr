525c7d8def2374792e3add84d2f4b5828e49d0d5
Dmytro Sereda <sereda21d@gmail.com>
2023-03-31 09:03:46 -0400

    Pagination logic enhanced

--- src/pages/view-entries/view-entries.component.tsx
+++ src/pages/view-entries/view-entries.component.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Entry from "../../components/entry/entry.component";
 import { Heading3, PageContainer } from "../../global";
+import { updateEntryBeingEdited } from "../../redux/helpers/helpers.reducer";
 import { selectEntries } from "../../redux/user/user.selectors";
-import { useAppSelector } from "../../utils/hooks";
+import { useAppDispatch, useAppSelector } from "../../utils/hooks";
 import {
   EntriesContainer,
   EntriesPerPageContainer,
@@ -16,34 +17,45 @@ import {
 
 const ViewEntriesPage: React.FC = () => {
   const [numberOfPages, setNumberOfPages] = useState(0);
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [limitEntriesPerPage, setLimitEntriesPerPage] = useState(5);
+  const [buttonsList, setButtonsList] = useState<any[]>([]);
 
   // GLOBAL STATE
   const entries = useAppSelector(selectEntries);
+  const dispatch = useAppDispatch();
 
   // Divide into pages
-  const buttonsList = [];
-  for (let i = 1; i <= numberOfPages; i++) {
-    buttonsList.push(
-      <PageButton
-        onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-          e.preventDefault();
-          setCurrentPage(i);
-        }}
-        key={i}
-        isActive={currentPage === i}
-      >
-        {i}
-      </PageButton>
-    );
-  }
 
   useEffect(() => {
     setNumberOfPages(Math.ceil(entries.length / limitEntriesPerPage));
-    setCurrentPage(1);
     // eslint-disable-next-line
-  }, []);
+  }, [entries]);
+
+  useEffect(() => {
+    const buttons = [];
+    for (let i = 1; i <= numberOfPages; i++) {
+      buttons.push(
+        <PageButton
+          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+            e.preventDefault();
+
+            // Disable editing mode
+            if (i !== currentPage) dispatch(updateEntryBeingEdited(""));
+
+            setCurrentPage(i);
+          }}
+          key={i}
+          isActive={currentPage === i}
+        >
+          {i}
+        </PageButton>
+      );
+    }
+
+    setButtonsList(buttons);
+    // eslint-disable-next-line
+  }, [numberOfPages, currentPage]);
 
   return (
     <PageContainer>
