294336876837bf9912d96673ede47d98431bdfa8
Dmytro Sereda <sereda21d@gmail.com>
2023-04-09 10:33:55 -0400

    Test

--- cypress/e2e/aaa.spec.ts
+++ cypress/e2e/aaa.spec.ts
@@ -1,10 +1,10 @@
 describe("Test", () => {
   it("Works", () => {
-    const projectId = Cypress.env("CYPRESS_PROJECT_ID");
+    const projectId = Cypress.env("PROJECT_ID");
     const test = Cypress.env("TEST_VARIABLE");
     const anotherTest = Cypress.env("ANOTHER_TEST_VARIABLE");
     expect(test).to.equal(200);
-    expect(anotherTest).to.equal("hello");
-    expect(projectId).to.equal("prsnlmgr");
+    expect(anotherTest).to.equal("prsnlmgr");
+    // expect(projectId).to.equal("prsnlmgr");
   });
 });
