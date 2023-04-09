3cfa40e9b2cdfd4765f05ed02439f4fdbbeb48c5
Dmytro Sereda <sereda21d@gmail.com>
2023-04-09 09:52:25 -0400

    Test

--- cypress/e2e/aaa.spec.ts
+++ cypress/e2e/aaa.spec.ts
@@ -1,8 +1,6 @@
 describe("Test", () => {
   it("Works", () => {
-    const test = Cypress.env("TEST_VARIABLE");
     const projectId = Cypress.env("PROJECT_ID");
-    expect(test).to.equal(21);
     expect(projectId).to.equal("prsnlmgr-713a4");
   });
 });
