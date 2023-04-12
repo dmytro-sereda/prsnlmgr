007311004f7e4d73cdbf84d88b225405bbdc3799
Dmytro Sereda <sereda21d@gmail.com>
2023-04-11 20:26:28 -0400

    Test

--- cypress/e2e/aac.spec.ts
+++ cypress/e2e/aac.spec.ts
@@ -1,6 +1,6 @@
 describe("first", () => {
   it("works", () => {
-    const testVar = Cypress.env("PROJECT_ID");
+    const testVar = Cypress.env("PROJECT_ID_TWO");
     expect(testVar).to.equal("prsnlmgr-713a4");
   });
 });
