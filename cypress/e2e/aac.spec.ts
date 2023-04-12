52aae6ae905ce053db0f1be5d877edd68e1e03ba
Dmytro Sereda <sereda21d@gmail.com>
2023-04-11 20:31:37 -0400

    Test

--- cypress/e2e/aac.spec.ts
+++
@@ -1,6 +0,0 @@
-describe("first", () => {
-  it("works", () => {
-    const testVar = Cypress.env("PROJECT_ID_TWO");
-    expect(testVar).to.equal("prsnlmgr-713a4");
-  });
-});
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -5,7 +5,7 @@ import "firebase/compat/database";
 import { attachCustomCommands } from "cypress-firebase";
 
 const apiKey = Cypress.env("FIREBASE_API_KEY");
-const projectId = Cypress.env("PROJECT_ID");
+const projectId = Cypress.env("FIREBASE_PROJECT_ID");
 const appId = Cypress.env("FIREBASE_APP_ID");
 
 const fbConfig = {
