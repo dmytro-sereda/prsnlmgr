6bac954c7a26bbecafb8d5c77dbaf81613e66854
Dmytro Sereda <sereda21d@gmail.com>
2023-04-12 22:42:40 -0400

    Test

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -10,10 +10,14 @@ const appId = Cypress.env("FIREBASE_APP_ID");
 
 const fbConfig = {
   apiKey,
-  authDomain: `${projectId}.firebaseapp.com`,
-  databaseURL: `https://${projectId}-default-rtdb.firebaseio.com`,
-  projectId,
-  storageBucket: `${projectId}.appspot.com`,
+  authDomain: `prsnlmgr-713a4.firebaseapp.com`,
+  // authDomain: `${projectId}.firebaseapp.com`,
+  databaseURL: `https://prsnlmgr-713a4-default-rtdb.firebaseio.com`,
+  // databaseURL: `https://${projectId}-default-rtdb.firebaseio.com`,
+  projectId: "prsnlmgr-713a4",
+  // projectId,
+  storageBucket: `prsnlmgr-713a4.appspot.com`,
+  // storageBucket: `${projectId}.appspot.com`,
   messagingSenderId: "508479827792",
   appId,
 };
